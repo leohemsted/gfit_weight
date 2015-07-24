2744b167df2ac2cc087db6897b8b4dc38f5525ea
Leo Hemsted <leo.hemsted@artsalliancemedia.com>
2015-07-24 13:19:08 +0100

    show my weight

--- javascript/main.js
+++ javascript/main.js
@@ -11,37 +11,56 @@ requirejs.config({
             'async': 'lib/requirejs-plugins/1.0.3/async.min',
             'propertyParser': 'lib/requirejs-plugins/1.0.3/propertyParser.min',
             'goog': 'lib/requirejs-plugins/1.0.3/goog.min',
-            'json': 'lib/require-text/2.0.12/text.min'
+            'json': 'lib/requirejs-plugins/1.0.3/json.min',
+            'text': 'lib/require-text/2.0.12/text.min'
         }
     }
 });
 
+function format_data(raw_data) {
+    var data = raw_data.point.map(function(point) {
+        // get it out of nanoseconds
+        var val = point.value[0].fpVal;
+
+        // start time is in nanos, date accepts timestamps in millis, so convert for tat
+        var time = new Date(+point.startTimeNanos / 1e6);
+        return [time, val];
+    });
+    data.sort(function(a, b) {
+        return a[0] < b[0];
+    })
+    return data;
+}
+
 requirejs(
     [
-        'goog!visualization,1,packages:[corechart]',
-        'json!../data.json'
+        'json!../data.json',
+        'goog!visualization,1,packages:[corechart]'
     ],
-    // google packages just shove themselves onto global scope, _goog is always undef :(
-    function (_goog, raw_data) {
+    // google doesn't have AMD so we need to access it via window.google
+    function (raw_data) {
+        var points = format_data(raw_data);
         var data = new window.google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-            ['Mushrooms', 3],
-            ['Onions', 1],
-            ['Olives', 1],
-            ['Zucchini', 1],
-            ['Pepperoni', 2]
-        ]);
+        data.addColumn('datetime', 'Date');
+        data.addColumn('number', 'Weight');
+        data.addRows(points);
 
         // Set chart options
         var options = {
-            'title': 'How Much Pizza I Ate Last Night',
-            'width': 400,
-            'height': 300
+            title: 'Leos Weight',
+            vAxis: {
+                title: 'Weight (kg)'
+            },
+            hAxis: {
+                title: 'Date',
+                formatType: 'short',
+                gridlines: {count: -1}
+            },
         };
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new window.google.visualization.PieChart(document.getElementById('chart_div'));
+        var chart = new window.google.visualization.ScatterChart(
+            document.getElementById('chart_div')
+        );
         chart.draw(data, options);
 });
