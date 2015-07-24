aa9eb66f0b923a9b5acff9caaedfd2b1868cd21d
Leo Hemsted <leo.hemsted@artsalliancemedia.com>
2015-07-24 14:01:11 +0100

    use moment to get a max and min a bit outside of the bounds of the points. also fix sorting

--- javascript/main.js
+++ javascript/main.js
@@ -8,54 +8,72 @@ requirejs.config({
     },
     map: {
         '*': {
+            'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js',
             'async': 'lib/requirejs-plugins/1.0.3/async.min',
             'propertyParser': 'lib/requirejs-plugins/1.0.3/propertyParser.min',
             'goog': 'lib/requirejs-plugins/1.0.3/goog.min',
             'json': 'lib/requirejs-plugins/1.0.3/json.min',
-            'text': 'lib/require-text/2.0.12/text.min'
+            'text': 'lib/require-text/2.0.12/text.min',
         }
     }
 });
 
-function format_data(raw_data) {
-    var data = raw_data.point.map(function(point) {
-        // get it out of nanoseconds
-        var val = point.value[0].fpVal;
-
-        // start time is in nanos, date accepts timestamps in millis, so convert for tat
-        var time = new Date(+point.startTimeNanos / 1e6);
-        return [time, val];
-    });
-    data.sort(function(a, b) {
-        return a[0] < b[0];
-    })
-    return data;
-}
-
 requirejs(
     [
         'json!../data.json',
+        'moment',
         'goog!visualization,1,packages:[corechart]'
     ],
     // google doesn't have AMD so we need to access it via window.google
-    function (raw_data) {
-        var points = format_data(raw_data);
+    function (raw_data, moment) {
+        var points = formatData(raw_data);
         var data = new window.google.visualization.DataTable();
+        var DAYS_EITHER_SIDE = 10;
+
+        function formatData(raw_data) {
+            var data = raw_data.point.map(function(point) {
+                // get it out of nanoseconds
+                var val = point.value[0].fpVal;
+
+                // start time is in nanos, date accepts timestamps in millis, so convert for tat
+                var time = new Date(+point.startTimeNanos / 1e6);
+                return [time, val];
+            });
+            return data.sort(function(a, b) {
+                return a[0] - b[0];
+            });
+        }
+
+        function getMinDate(points) {
+            return moment(points[0][0]).subtract(DAYS_EITHER_SIDE, 'days').toDate();
+        }
+        function getMaxDate(points) {
+            return moment(points.slice(-1)[0][0]).add(DAYS_EITHER_SIDE, 'days').toDate();
+        }
+
         data.addColumn('datetime', 'Date');
         data.addColumn('number', 'Weight');
         data.addRows(points);
 
         // Set chart options
         var options = {
-            title: 'Leos Weight',
+            title: 'Weight',
             vAxis: {
                 title: 'Weight (kg)'
             },
             hAxis: {
                 title: 'Date',
-                formatType: 'short',
-                gridlines: {count: -1}
+                gridlines: {count: -1},
+                viewWindowMode: 'explicit',
+                viewWindow: {
+                    // dates are zero indexed just because
+                    min: getMinDate(points),
+                    max: getMaxDate(points),
+
+                    // the chart deals only in
+                },
             },
+            legend: {position: 'none'},
         };
 
         // Instantiate and draw our chart, passing in some options.
